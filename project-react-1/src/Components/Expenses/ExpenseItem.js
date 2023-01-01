367382bdf4efe3ba27bd28aaead197c960ce13c7
WaleedAdnan08 <bsef21m546@pucit.edu.pk>
2023-01-01 16:08:53 +0500

    Function Added

--- project-react-1/src/Components/Expenses/ExpenseItem.js
+++ project-react-1/src/Components/Expenses/ExpenseItem.js
@@ -8,6 +8,7 @@ const ExpenseItem=(props)=> {
             <h2>{props.title}</h2>
             <div className='expense-item__price'>{props.amount}</div>
         </div>
+        
     </Card>
 }
 export default ExpenseItem;
--- project-react-2/src/Components/Expenses/ExpenseItem.js
+++ project-react-2/src/Components/Expenses/ExpenseItem.js
@@ -1,13 +1,17 @@
+import React from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
-const ExpenseItem=(props)=> {
+const ExpenseItem = (props) => {
+const clickHandler=()=>console.log('Clicked!!!');
     return <Card className='expense-item'>
-        <ExpenseDate date={props.date}/> {/*This is a self closing tag, same as <ExpenseItem></ExpenseItem>*/}
+        <ExpenseDate date={props.date} /> {/*This is a self closing tag, same as <ExpenseItem></ExpenseItem>*/}
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
             <div className='expense-item__price'>{props.amount}</div>
         </div>
-    </Card>
+        <button onClick={clickHandler}>Change Title</button>
+    </Card> 
+  
 }
 export default ExpenseItem;
