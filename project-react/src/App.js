694bcb8d2f838c8dab47c677ccd19a30c18fa53e
WaleedAdnan08 <bsef21m546@pucit.edu.pk>
2023-01-01 12:53:16 +0500

    1st p done and commited

--- project-react/src/App.js
+++ project-react/src/App.js
@@ -1,8 +1,8 @@
-import ExpenseItem from './Components/ExpenseItem';
-
+import ExpenseItem from './Components/Expenses/ExpenseItem';
+import Card from '../src/Components/UI/Card';
 import './App.css';
 
-function App() {
+const App=()=> {
   const expenses = [
     {
       id: 'e1',
@@ -30,13 +30,13 @@ function App() {
     },
   ];
   return (
-    <div>
+    <Card>
       <h2>Lets get started</h2>
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
       <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
-    </div>
+    </Card>
   );
 }
 export default App;
--- project-react/src/Components/Expenses/ExpenseItem.js
+++ project-react/src/Components/Expenses/ExpenseItem.js
@@ -1,12 +1,13 @@
+import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
-function ExpenseItem(props) {
-    return <div className='expense-item'>
+const ExpenseItem=(props)=> {
+    return <Card className='expense-item'>
         <ExpenseDate date={props.date}/> {/*This is a self closing tag, same as <ExpenseItem></ExpenseItem>*/}
         <div className='expense-item__description'>
             <h2>{props.title}</h2>
             <div className='expense-item__price'>{props.amount}</div>
         </div>
-    </div>
+    </Card>
 }
 export default ExpenseItem;
