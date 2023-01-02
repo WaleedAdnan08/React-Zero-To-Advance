00f40ef381715258e0f12c48da7815cd5fb3ddc0
WaleedAdnan08 <bsef21m546@pucit.edu.pk>
2023-01-02 10:10:39 +0500

    States
    Multiple states added in code for individual triggers

--- project-react-2/src/Components/Expenses/ExpenseItem.js
+++ project-react-2/src/Components/Expenses/ExpenseItem.js
@@ -1,23 +1,29 @@
 import React, { useState } from 'react';
-import Card from '../UI/Card';
-import ExpenseDate from './ExpenseDate';
 
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
+
 const ExpenseItem = (props) => {
-    const [title, setTitle] = useState(props.title);
-    const clickHandler = () => {
-        setTitle('Updated!');
-        console.log(title);
-    };
-    return( 
+  // function clickHandler() {}
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+  
+  const clickHandler = () => {
+    setTitle('Updated!');
+    console.log(title);
+  };
+
+  return (
     <Card className='expense-item'>
-        <ExpenseDate date={props.date} /> {/*This is a self closing tag, same as <ExpenseItem></ExpenseItem>*/}
-        <div className='expense-item__description'>
-            <h2>{title}</h2>
-            <div className='expense-item__price'>{props.amount}</div>
-        </div>
-        <button onClick={clickHandler}>Change Title</button>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
-);
+  );
 }
+
 export default ExpenseItem;
