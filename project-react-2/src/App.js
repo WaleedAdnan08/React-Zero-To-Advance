e2ace535621994e268a02014d4178003994d5dc1
WaleedAdnan08 <bsef21m546@pucit.edu.pk>
2023-01-09 19:31:40 +0500

    UnNeeded project deleted

--- project-react-2/src/App.js
+++ project-react-2/src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
+import NewExpense from './Components/NewExpense/NewExpense';
+import Expenses from './Components/Expenses/Expenses'
+// import NewExpense from './components/NewExpense/NewExpense';
+// import Expenses from './components/Expenses/Expenses';
 
 const DUMMY_EXPENSES = [
   {
