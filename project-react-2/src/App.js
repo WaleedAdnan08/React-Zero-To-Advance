fbf43a61e38d2a37333f74bff4be2e3dc73dc1b9
WaleedAdnan08 <bsef21m546@pucit.edu.pk>
2023-01-09 20:17:51 +0500

    Expenses

--- project-react-2/src/App.js
+++ project-react-2/src/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
+
 import NewExpense from './Components/NewExpense/NewExpense';
 import Expenses from './Components/Expenses/Expenses'
-// import NewExpense from './components/NewExpense/NewExpense';
-// import Expenses from './components/Expenses/Expenses';
 
 const DUMMY_EXPENSES = [
   {
--- project-react-2/src/Components/Expenses/Expenses.js
+++ project-react-2/src/Components/Expenses/Expenses.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
@@ -10,17 +11,30 @@ const Expenses = (props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className='expenses'>
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date} />
-        ))}
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {filteredExpenses.length === 0 ? (
+          <p>No expenses found.</p>) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date} 
+            />
+          )
+          ))}
+
       </Card>
     </div>
   );
