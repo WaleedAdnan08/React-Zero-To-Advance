4ac2263870e81e50d4062d939e52ab55218bc4fe
WaleedAdnan08 <bsef21m546@pucit.edu.pk>
2023-01-10 18:49:25 +0500

    ChartBar updated

--- project-react-2/src/Components/Expenses/Expenses.js
+++ project-react-2/src/Components/Expenses/Expenses.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
-import ExpensesList from '../ExpensesList';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
--- project-react-2/src/Components/Expenses/ExpensesList.js
+++ project-react-2/src/Components/Expenses/ExpensesList.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import ExpenseItem from './Expenses/ExpenseItem';
+import ExpenseItem from './ExpenseItem';
 import './ExpensesList.css';
 
 const ExpensesList = (props) => {
-    let expensesContent = <p>No expenses found.</p>
+
         if(props.items.length === 0){
-            return <h2 className='expense-list__fallback'>Found no expenses</h2>
+            return <h2 className='expenses-list__fallback'>Found no expenses</h2>
         }
     return <ul className='expenses-list'>
-        props.items.map((expense) => (
+        {props.items.map((expense) => (
         <ExpenseItem
             key={expense.id}
-            title={expense.title}
+            title={expense.title} 
             amount={expense.amount}
             date={expense.date}
         />
-        ))
+        ))}
     </ul>
 };
 export default ExpensesList;
